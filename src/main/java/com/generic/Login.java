fb7aa696078f3270ad8b5faf1e3009009ccdbbe0
shamima30 <nasrin.shamima30@gmail.com>
2023-11-17 23:08:17 -0500

    first save

---
+++ src/main/java/com/generic/Login.java
@@ -0,0 +1,22 @@
+package com.generic;
+
+public class Login {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		int a = 10;
+		
+int age = 50 ;
+String name = "shamima";
+		
+		
+		System.out.println(age);
+		System.out.println(a);
+		System.out.println(name);
+	}
+	
+
+}
